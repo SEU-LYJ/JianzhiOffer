18783caa940d7da67e5e306e2cd8633ff5e613a2
SEU-LYJ <704232442@qq.com>
2018-04-19 15:11:06 +0800

    Add a complete answer to interview problem 6.

---
+++ interView-6.cpp
@@ -0,0 +1,64 @@
+#include<iostream>
+#include<vector>
+#include<stack>
+using namespace std;
+struct ListNode
+{
+	int val;
+	ListNode *next;
+	ListNode(int x) :val(x),next(nullptr) {};
+	void AddList(int NodeVal)
+	{
+		ListNode *temp;
+		temp=this;
+		while (temp->next != nullptr) temp = temp->next;
+		ListNode *pNew=new ListNode(NodeVal);
+		temp->next = pNew;
+
+	}
+};
+vector<int> printListFromTailToHead(ListNode* head)
+{
+	vector<int> PrintValues;
+	stack<int> TempValues;
+	while (head != nullptr)
+	{
+		TempValues.push(head->val);
+		head = head->next;
+	}
+	while (!TempValues.empty())
+	{
+		PrintValues.push_back(TempValues.top());
+		TempValues.pop();
+	}
+	return PrintValues;
+}
+class Solution {
+public:
+	vector<int> printListFromTailToHead(ListNode* head) 
+	{
+		vector<int> PrintValues;
+		stack<int> TempValues;
+		while (head != nullptr)
+		{
+			TempValues.push(head->val);
+			head = head->next;
+		}
+		while (!TempValues.empty())
+		{
+			PrintValues.push_back(TempValues.top());
+			TempValues.pop();
+		}
+		return PrintValues;
+	}
+};
+int main()
+{
+	ListNode A(1);
+	A.AddList(2);
+	A.AddList(3);
+	vector<int> OutPutList = printListFromTailToHead(&A);
+	for (auto tempNode = OutPutList.begin(); tempNode < OutPutList.end(); tempNode++) cout << (*tempNode) << endl;
+	return 0;
+}
+
